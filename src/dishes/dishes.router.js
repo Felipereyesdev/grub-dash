2f7def31ebaeab166fe2ec3c89c0abd7fed92293
Felipe Reyes <felipereyes3004@gmail.com>
2022-07-18 12:17:05 -0400

    all test for dish are passing

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const controller = require("./dishes.controller");
+const methodNotAllow = require("../errors/methodNotAllowed")
 
 // TODO: Implement the /dishes routes needed to make the tests pass
-router.route("/:dishId").get(controller.read);
-router.route("/").get(controller.list).post(controller.create);
+router.route("/:dishId").get(controller.read).put(controller.update).all(methodNotAllow)
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllow);
 
 
 module.exports = router;
